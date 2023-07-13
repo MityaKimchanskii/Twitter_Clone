68c9f20a4b0a07349d7b0acdfb9165d8d880e4bb
Dmitrii Kim <mitya_kim@icloud.com>
2023-07-12 19:52:53 -0700

    feat: Add Registration View

--- Twitter/Twitter/Core/Authentication/Views/RegistrationView.swift
+++ Twitter/Twitter/Core/Authentication/Views/RegistrationView.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var username = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            VStack(alignment: .leading) {
+                HStack { Spacer() }
+                
+                Text("Get started")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text("Create your account")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height: 260)
+            .padding(.leading)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(RoundedCorner(corners: [.bottomRight]))
+            
+            VStack(spacing: 39) {
+                CustomInputField(imageName: "envelope", placeholderText: "Email", text: $email)
+                
+                CustomInputField(imageName: "person", placeholderText: "Username", text: $username)
+                
+                CustomInputField(imageName: "person", placeholderText: "Full name", text: $fullName)
+                
+                CustomInputField(imageName: "lock", placeholderText: "Password", text: $password)
+            }
+            .padding(32)
+            
+            
+            Button {
+            
+            } label: {
+                Text("Sign Up")
+                    .font(.headline)
+                    .foregroundColor(Color.white)
+                    .frame(width: 330, height: 45)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
+            
+            Spacer()
+            
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                HStack {
+                    Text("Already have an account?")
+                        .font(.footnote)
+                    
+                    Text("Sign In")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+        }
+        .ignoresSafeArea()
     }
 }
 
