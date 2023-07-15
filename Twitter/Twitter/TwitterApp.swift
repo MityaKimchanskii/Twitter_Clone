a3650305f1dfada230585f2ceb9fee0963616d04
Dmitrii Kim <mitya_kim@icloud.com>
2023-07-14 20:22:06 -0700

    feat: Add Firebase

--- Twitter/Twitter/TwitterApp.swift
+++ Twitter/Twitter/TwitterApp.swift
@@ -6,13 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ContentView()
+                LoginView()
             }
         }
     }
