bc78d2192ca7bdc80cf3190f8aa7cc42bf3e395b
Dmitrii Kim <mitya_kim@icloud.com>
2023-07-23 22:18:58 -0700

    feat: Add search functionality to ExploreView

--- Twitter/Twitter/Core/Explore/ViewModels/ExplereViewModel.swift
+++ Twitter/Twitter/Core/Explore/ViewModels/ExplereViewModel.swift
@@ -10,6 +10,16 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
+    
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercaseQuery = searchText.lowercased()
+            return users.filter({ $0.username.contains(lowercaseQuery) || $0.fullname.lowercased().contains(lowercaseQuery) })
+        }
+    }
     
     let service = UserService()
     
